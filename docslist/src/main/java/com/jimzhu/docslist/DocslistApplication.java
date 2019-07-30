83fc409cb9c227b85891b2903c7ee435a34c08bf
Zhu,Jim <jim.zhu@fhlbny.com>
2019-07-30 16:52:39 -0400

    initial

---
+++ docslist/src/main/java/com/jimzhu/docslist/DocslistApplication.java
@@ -0,0 +1,13 @@
+package com.jimzhu.docslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocslistApplication.class, args);
+	}
+
+}
