148acf185f91e2d0523a132b6b1cbb83b10a955d
Zhu,Jim <jim.zhu@fhlbny.com>
2019-07-31 16:06:00 -0400

    update

---
+++ docslist/src/main/java/com/jimzhu/docslist/controller/DocsHome.java
@@ -0,0 +1,15 @@
+package com.jimzhu.docslist.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DocsHome {
+	
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+}
