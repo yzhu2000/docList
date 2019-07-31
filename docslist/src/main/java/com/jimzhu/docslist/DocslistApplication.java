f2337dbce22d1e41ac4172b818157d33aebe391b
Zhu,Jim <jim.zhu@fhlbny.com>
2019-07-31 16:04:45 -0400

    update

--- docslist/src/main/java/com/jimzhu/docslist/DocslistApplication.java
+++ docslist/src/main/java/com/jimzhu/docslist/DocslistApplication.java
@@ -1,9 +1,11 @@
 package com.jimzhu.docslist;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DocslistApplication {
 
 	public static void main(String[] args) {
