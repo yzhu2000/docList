5ce493190854ee6b0b0efc44efd2fc433b7447e0
yzhu2000 <User@DESKTOP-8HH5E01.home>
2019-08-04 01:41:33 -0400

    add junit test

--- docslist/src/test/java/com/jimzhu/docslist/repository/ArticleRepositoryTest.java
+++ docslist/src/test/java/com/jimzhu/docslist/repository/ArticleRepositoryTest.java
@@ -1,14 +1,31 @@
 package com.jimzhu.docslist.repository;
 
+
 import static org.junit.Assert.*;
-
+import java.util.List;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.jimzhu.docslist.model.Article;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ArticleRepositoryTest {
-
+	
+	@Autowired
+	private ArticleRepository articleRepository; 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getTest() {
+		
+		List<Article> articles = articleRepository.findAll();
+		assertFalse(articles.isEmpty());
+				
+		
 	}
 
 }
